547c8a8e1dab2c947e726d8cdb27129f4024de24
dukesx <afzaalbci@ymail.com>
2021-10-10 11:25:36 +0500

    fix(app): :bug: fixes class issue
    #31

--- index.ts
+++ index.ts
@@ -2,10 +2,9 @@ console.log("hello world");
 
 class Hello {
   nama: string = "";
-  //TODO: #14 Fix this please @dukesx
   constructor(nama: string) {
     this.nama = nama;
   }
 }
 
-var jss = new Hello("World Exclusive");
+var jss = new Hello("");
